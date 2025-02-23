fc61b9165abb414c9c484f2de2e3d53d97d455a8
TelmoK <telmo.kiss@gmail.com>
2025-02-23 17:19:56 +0100

    Preparing the Game class for the use of states

--- P1/src/game/Game.h
+++ P1/src/game/Game.h
@@ -3,6 +3,8 @@
 #pragma once
 #include "../utils/Singleton.h"
 
+#include "GameState.h"
+
 namespace ecs {
 class Manager;
 }
@@ -16,6 +18,35 @@ public:
 	void start();
 	ecs::Manager* getMngr() { return _mngr; }
 
+	enum State {
+		RUNNING, PAUSED, NEWGAME, NEWROUND, GAMEOVER
+	};
+
+	inline void setState(State s) {
+		_state->leave();
+		switch (s) {
+		case RUNNING:
+			_state = _runing_state;
+			break;
+		case PAUSED:
+			_state = _paused_state;
+			break;
+		case NEWGAME:
+			_state = _newgame_state;
+			break;
+		case NEWROUND:
+			_state = _newround_state;
+			break;
+		case GAMEOVER:
+			_state = _gameover_state;
+			break;
+		default:
+			break;
+		}
+		_state->enter();
+	}
+
+
 private:
 
 	friend Singleton<Game>;
@@ -24,6 +55,14 @@ private:
 	virtual ~Game();
 
 	void checkCollisions();
+
 	ecs::Manager *_mngr;
+
+	GameState* _state;
+	GameState* _paused_state;
+	GameState* _runing_state;
+	GameState* _newgame_state;
+	GameState* _newround_state;
+	GameState* _gameover_state;
 };
 
